b84ba3a0a1a106a038547ab6ba4df62c25fdb534
Bharath Raam <bharathsrini19@gmail.com>
2023-12-03 18:24:35 -0500

    initial

---
+++ src/main/java/com/example/testmircoservice/TestMircoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmircoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMircoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMircoserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testmircoservice/TestMircoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmircoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMircoserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
